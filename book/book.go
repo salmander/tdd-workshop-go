d72d43ee9ac3278a21a08db3cdab8de3a6059287
Mark Bradley <mark.bradley@sainsburys.co.uk>
2017-10-15 15:32:11 +0100

    Mock Test Double Task
    Creating task 4 task code. The task demonstrates the use of mock test doubles.
    To complete the task you must creat the mock object require to make the BookService test to pass.

--- book/book.go
+++ book/book.go
@@ -1,6 +1,9 @@
 package book
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // START OF VENDOR CODE
 
@@ -18,29 +21,84 @@ type Book struct {
 	yearPublished int
 }
 
-type Review struct {
-	rating      int
-	comment     string
-	name        string
-	timeCreated time.Time
+func NewBook(isbn, ean, title, author string, price float32, yearPublished int) Book {
+	return Book{
+		isbn:          isbn,
+		ean:           ean,
+		title:         title,
+		author:        author,
+		price:         price,
+		yearPublished: yearPublished,
+	}
 }
 
 type BookRepository interface {
 	findByISBN(isbn string) (Book, error)
 }
 
-type ReviewRepostiory interface {
-	findByEAN(ean string) ([]Review, error)
-}
-
 // END OF VENDOR CODE
 
-// You need to implement the following interface using TDD
+// IMPLEMENTATION
 
-type BookService interface {
-	// Both functions eturns JSON no matter the response from the service
-	getBook(isbn string) string
-	getBookWithReviews(isbn string) string
+type BookService struct {
+	BookRepository BookRepository
 }
 
-// Your code here!
+type BookJSON struct {
+	Isbn          string  `json:"isbn"`
+	Ean           string  `json:"ean"`
+	Title         string  `json:"title"`
+	Author        string  `json:"author"`
+	Price         float32 `json:"price"`
+	YearPublished int     `json:"year_published"`
+}
+
+type ErrorJSON struct {
+	Status int    `json:"status"`
+	Code   int    `json:"code"`
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
+
+func (bs BookService) getBook(isbn string) string {
+	book, err := bs.BookRepository.findByISBN(isbn)
+
+	if err != nil {
+
+		var errorJSON ErrorJSON
+		if err.Error() == "NO BOOK" {
+			errorJSON = ErrorJSON{
+				Status: 404,
+				Code:   1,
+				Title:  "Book not found",
+				Detail: "Could not find book with ISBN: " + isbn,
+			}
+		} else {
+			errorJSON = ErrorJSON{
+				Status: 500,
+				Code:   2,
+				Title:  "Repository unavailable",
+				Detail: "Service unavailable, try again later",
+			}
+		}
+
+		jsonString, _ := json.Marshal(errorJSON)
+
+		return string(jsonString)
+	}
+
+	bookJSON := BookJSON{
+		Isbn:          book.isbn,
+		Ean:           book.ean,
+		Title:         book.title,
+		Author:        book.author,
+		Price:         book.price,
+		YearPublished: book.yearPublished,
+	}
+
+	fmt.Printf("bookJSON: %v", bookJSON)
+
+	jsonString, _ := json.Marshal(bookJSON)
+
+	return string(jsonString)
+}
--- book/book_test.go
+++ book/book_test.go
@@ -1,3 +1,39 @@
 package book
 
-// Remember to test Happy and Sad paths.
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBookServiceReturnsValidJSONWhenBookFound(t *testing.T) {
+	drNoJSON := `{"isbn":"0099576929","ean":"1357924680","title":"Dr No","author":"Ian Fleming","price":3.99,"year_published":1993}`
+	casinoRoyaleJSON := `{"isbn":"0230037496","ean":"2468013579","title":"Casino Royale","author":"Ian Fleming","price":4.99,"year_published":1990}`
+
+	drNo := NewBook("0099576929", "1357924680", "Dr No", "Ian Fleming", 3.99, 1993)
+	casinoRoyale := NewBook("0230037496", "2468013579", "Casino Royale", "Ian Fleming", 4.99, 1990)
+
+	bookService := BookService{bookRepository}
+
+	assert.Equal(t, drNoJSON, bookService.getBook("0099576929"))
+	assert.Equal(t, casinoRoyaleJSON, bookService.getBook("0230037496"))
+}
+
+func TestBookServiceReutrnsValidJsonWhenBookNotFound(t *testing.T) {
+	errorMoonrakerJSON := `{"status":404,"code":1,"title":"Book not found","detail":"Could not find book with ISBN: 0099576872"}`
+	errorGoldfingerJSON := `{"status":404,"code":1,"title":"Book not found","detail":"Could not find book with ISBN: 1784871095"}`
+
+	bookService := BookService{bookRepository}
+
+	assert.Equal(t, errorMoonrakerJSON, bookService.getBook("0099576872"))
+	assert.Equal(t, errorGoldfingerJSON, bookService.getBook("1784871095"))
+}
+
+func TestBookServiceReturnValidJsonWhenBookRepositoryUnavaiable(t *testing.T) {
+	errorRepositoryJSON := `{"status":500,"code":2,"title":"Repository unavailable","detail":"Service unavailable, try again later"}`
+
+	bookService := BookService{bookRepository}
+
+	assert.Equal(t, errorRepositoryJSON, bookService.getBook("009957702X"))
+
+}
